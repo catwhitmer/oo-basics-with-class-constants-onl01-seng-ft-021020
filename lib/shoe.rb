143b509ca49f2e7c489102627eb92ed4037343d9
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-18 19:06:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,11 +6,16 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+<<<<<<< HEAD
     if BRANDS.include?(brand)
     
     else
       BRANDS << brand
     end
+=======
+    @brand.any? {|x| x == brand}
+  brand << BRANDS
+>>>>>>> a730f9f0529680f980cdfb4ddd40e5fe3c0dfe56
   end
 
 end
